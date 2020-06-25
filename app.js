8f8d495ed1dc2c00a3d0ca6396799fc07c92409b
dulciando <yearsafterodeo@gmail.com>
2020-06-25 18:09:21 +0300

    🏋️

---
+++ app.js
@@ -0,0 +1,49 @@
+gsap.from(".logo", 1.6, {
+    delay: 6.4,
+    opacity:0,
+    y:30,
+    ease: Expo.easeInOut
+});
+
+gsap.from(".menu", 1.6, {
+    delay: 6.5,
+    opacity:0,
+    y:30,
+    ease: Expo.easeInOut
+});
+
+gsap.from(".heading", 1.6, {
+    delay: 6.6,
+    opacity:0,
+    y:30,
+    ease: Expo.easeInOut
+});
+
+gsap.from(".desc", 1.6, {
+    delay: 6.7,
+    opacity:0,
+    y:30,
+    ease: Expo.easeInOut
+});
+
+gsap.from("button", 1.6, {
+    delay: 6.8,
+    opacity:0,
+    y:30,
+    ease: Expo.easeInOut
+});
+
+gsap.from(".watch", 1.6, {
+    delay: 6.9,
+    opacity:0,
+    y:30,
+    rotation:90,
+    ease: Expo.easeInOut
+});
+
+gsap.staggerFrom(".media ul li", 2, {
+    delay: 7,
+    opacity:0,
+    y:40,
+    ease: Expo.easeInOut
+}, 0.2);
